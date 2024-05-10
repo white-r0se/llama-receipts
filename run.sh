d279eca316bff18447414eb9d0cd30cd056eab3b
Alexei Khokhulin <56470452+white-r0se@users.noreply.github.com>
2024-05-10 20:07:02 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-poetry run python -m src.cli train \
-    --base-model "unsloth/llama-3-8b-Instruct" \
-    --data-path "dialogues/receipts_dataset.jsonl" \
-    --output-dir "models/" \
-    --batch-size 1 \
-    --micro-batch-size 1 \
-    --num-epochs 1 \
-    --wandb-run-name receipts-llama
